e74d1e91761411525e3ff702423c4a18cd63e433
Qti3e <Alirezaghadimy@gmail.com>
2016-07-01 17:29:53 +0430

    Add admin templates

--- server/lib/hipo/admin.php
+++ server/lib/hipo/admin.php
@@ -18,6 +18,7 @@
  *                             Created by  Qti3e                             *
  *        <http://Qti3e.Github.io>    LO-VE    <Qti3eQti3e@Gmail.com>        *
  *****************************************************************************/
+
 namespace lib\hipo;
 use lib\client\js;
 use lib\client\sender;
