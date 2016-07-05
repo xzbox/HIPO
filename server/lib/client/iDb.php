cad4fe24b65c337eadffba39a61a2d5118940c4a
Qti3e <Alirezaghadimy@gmail.com>
2016-07-05 10:53:09 +0430

    manage user's iDb from server

---
+++ server/lib/client/iDb.php
@@ -0,0 +1,79 @@
+<?php
+/*****************************************************************************
+ *         In the name of God the Most Beneficent the Most Merciful          *
+ *___________________________________________________________________________*
+ *   This program is free software: you can redistribute it and/or modify    *
+ *   it under the terms of the GNU General Public License as published by    *
+ *   the Free Software Foundation, either version 3 of the License, or       *
+ *   (at your option) any later version.                                     *
+ *___________________________________________________________________________*
+ *   This program is distributed in the hope that it will be useful,         *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of          *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           *
+ *   GNU General Public License for more details.                            *
+ *___________________________________________________________________________*
+ *   You should have received a copy of the GNU General Public License       *
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.   *
+ *___________________________________________________________________________*
+ *                             Created by  Qti3e                             *
+ *        <http://Qti3e.Github.io>    LO-VE    <Qti3eQti3e@Gmail.com>        *
+ *****************************************************************************/
+namespace lib\client;
+
+/**
+ * Class iDb
+ * @package lib\client
+ */
+class iDb{
+	/**
+	 * @param $user
+	 * @param $key
+	 * @param $value
+	 *
+	 * @return int
+	 */
+	public static function set($user,$key,$value){
+		return sender::ByUser($user,'i1'.json_encode(['key'=>$key,'val'=>$value]));
+	}
+
+	/**
+	 * @param $user
+	 * @param $key
+	 *
+	 * @return int
+	 */
+	public static function incr($user,$key){
+		return sender::ByUser($user,'i2'.json_encode(['key'=>$key]));
+	}
+
+	/**
+	 * @param $user
+	 * @param $key
+	 * @param $value
+	 *
+	 * @return int
+	 */
+	public static function incrBy($user,$key,$value){
+		return sender::ByUser($user,'i3'.json_encode(['key'=>$key,'val'=>$value]));
+	}
+
+	/**
+	 * @param $user
+	 * @param $key
+	 *
+	 * @return int
+	 */
+	public static function del($user,$key){
+		return sender::ByUser($user,'i4'.json_encode(['key'=>$key]));
+	}
+
+	/**
+	 * @param $user
+	 * @param $input
+	 *
+	 * @return int
+	 */
+	public static function set_json($user,$input){
+		return sender::ByUser($user,'i5'.$input);
+	}
+}
