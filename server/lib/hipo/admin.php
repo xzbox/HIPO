4cfb1cab7b98832b7c0266cdb894ea205f166ca9
Qti3e <Alirezaghadimy@gmail.com>
2016-07-08 06:26:53 +0430

    admin::createContest

--- server/lib/hipo/admin.php
+++ server/lib/hipo/admin.php
@@ -20,87 +20,20 @@
  *****************************************************************************/
 
 namespace lib\hipo;
-use lib\client\js;
-use lib\client\sender;
+
+use lib\database\DB;
+
 class admin{
-	private static $pages = array();
-	private static $md5   = '';
-	private static $jsCode= '';
-
-	/**
-	 * @param $content
-	 *
-	 * @return mixed
-	 */
-	private static function min($content){
-		return preg_replace('/\s+(\S+)\s+/',' $1 ',$content);
-	}
-
-	/**
-	 * @return void
-	 */
-	public static function load(){
-		$md5   = md5('');
-		$DB    = array();
-		$tem   = glob('templates/admin/*.html');
-		$count = count($tem);
-		for($i = 0;$i < $count;$i++){
-			$page   = 'pages\\' . substr(basename($tem[$i]), 0, -5);
-			self::$pages[$page]         = self::min(file_get_contents($tem[$i]));
-			$DB['template_page_'.$page] = self::$pages[$page];
-			$md5    = md5($md5.self::$pages[$page].$page);
-		}
-		$pages = glob('pages/admin/*.php');
-		$count = count($pages);
-		for($i = 0;$i < $count;$i++){
-			/**
-			 * @type \lib\view\view
-			 */
-			$class = 'pages\\admin\\' . substr(basename($pages[$i]), 0, -4);
-			$class::load();
-			self::$pages[$class] = self::min($class::getTemplate($class));
-			$DB['template_page_pages\\' . substr(basename($pages[$i]), 0, -4)] = self::$pages[$class];
-			$md5 = md5($md5 . self::$pages[$class]);
-		}
-		$DB    = json_encode($DB);
-		self::$jsCode = js::jsFunc('iDb.SET_JSON',[$DB]);
-		self::$md5 = $md5;
-	}
-
-	/**
-	 * @param $name
-	 *
-	 * @return bool
-	 */
-	public static function get($name){
-		if(isset(self::$pages[$name])){
-			return self::$pages[$name];
+	public static function createContest($values){
+		if(isset($values['name']) && $values['level'] && $values['start'] && $values['time']){
+			DB::INCR('contestId');
+			$id = DB::GET('contestId');
+			DB::hset('contests','name',$values['name']);
+			DB::hset('contests','level',$values['level']);
+			DB::hset('contests','start',$values['start']);
+			DB::hset('contests','end',$values['start'] + $values['time']);
+			return true;
 		}
 		return false;
-	}
-
-	/**
-	 * @return mixed
-	 */
-	public static function md5(){
-		return self::$md5;
-	}
-
-	/**
-	 * @return string
-	 */
-	public static function jsCode(){
-		return self::$jsCode;
-	}
-
-	/**
-	 * @param $user
-	 *
-	 * @return void
-	 */
-	public static function sendAdminTemplate($user){
-		js::doFunc($user,'iDb.removeTemplate');
-		sender::ByUser($user,self::jsCode());
-		js::doFunc($user,'iDb.set',['templateHash',admin::md5()]);
 	}
 }
