e71cdf668311d1fb8bcb6b9eaf9f723edf1bc3f1
Qti3e <Alirezaghadimy@gmail.com>
2016-07-12 16:54:18 +0430

    Making xdb map & overview

--- client/assets/js/xdb.js
+++ client/assets/js/xdb.js
@@ -22,8 +22,15 @@ var xdb = (function(document,window){
     var indexDb             = window.indexedDB || window.mozIndexedDB || window.webkitIndexedDB || window.msIndexedDB || window.shimIndexedDB;
     var dbName              = 'WebSocket_'+appName;
     var open                = indexDb.open(dbName,1);
+    var db                  = null;
+    var usersTx,usersStore,usersIndex,
+        contestsTx,contestsStore,contestsIndex,
+        questionsTx,questionsStore,questionsIndex,
+        logTx,logStore,logIndex,
+        messagesTx,messagesStore,messagesIndex,
+        langsTx,langsStore,langsIndex;
     open.onupgradeneeded    = function(){
-        var db      = open.result;
+        db      = open.result;
         var store;
         var index;
         store       = db.createObjectStore('users',{keyPath: "id"});
@@ -64,23 +71,167 @@ var xdb = (function(document,window){
         store.createIndex("message", "message", { unique: false });
         store.createIndex("contest", "contest", { unique: false });
         store.createIndex("question", "question", { unique: false });
+
+        store       = db.createObjectStore('langs',{keyPath: "id"});
+        store.createIndex('key','key',{unique:true});
+        store.createIndex('value','value',{unique:false});
+
+        if(debug){
+            console.log('Database upgraded successfully.')
+        }
     };
-    publicFunctions.set     = function(key,value){
+    open.onsuccess          = function(){
+        db          = open.result;
+        usersTx     = db.transaction('users','readwrite');
+        contestsTx  = db.transaction('contests','readwrite');
+        questionsTx = db.transaction('questions','readwrite');
+        logTx       = db.transaction('log','readwrite');
+        messagesTx  = db.transaction('messages','readwrite');
+        langsTx     = db.transaction('langs','readwrite');
+
+        usersStore      = usersTx.objectStore('users');
+        contestsStore   = contestsTx.objectStore('contests');
+        questionsStore  = questionsTx.objectStotre('questions');
+        logStore        = logTx.objectStore('log');
+        messagesStore   = messagesTx.objectStore('messages');
+        langsStore      = langsTx.objectStore('langs');
+
+        usersIndex      = usersStore.index('username');
+        contestsIndex   = contestsIndex.index('name');
+        questionsIndex  = questionsStore;
+        logIndex        = logStore;
+        messagesStore   = messagesIndex;
+        langsStore      = langsIndex.index('key');
+
+        if(debug){
+            console.log('Database opened successfully.');
+        }
+    };
+    publicFunctions.users           = Object();
+    publicFunctions.users.new       = function(username,email,age,fname,lname,score,tSends,wSends,bio){
+        var event = usersStore.put({
+            'username':username,
+            'email':email,
+            'age':age,
+            'fname':fname,
+            'lname':lname,
+            'score':score,
+            'tSends':tSends,
+            'wSends':wSends,
+            'bio':bio
+        });
+        var re   = Object();
+        re.onEnd = function (func) {
+            event.onsuccess(func);
+        };
+        return re;
+    };
+    publicFunctions.users.set       = function(username,key,value){
+        var event = usersStore.put({
+            'username':username,
+            key:value
+        });
+        var re   = Object();
+        re.onEnd = function (func) {
+            event.onsuccess(func);
+        };
+        return re;
+    };
+    publicFunctions.users.get       = function(username){
 
     };
-    publicFunctions.get     = function(key){
+    publicFunctions.users.getById   = function(id){
 
     };
-    publicFunctions.del     = function(key){
+    publicFunctions.users.list      = function(){
 
     };
-    publicFunctions.isset   = function (key) {
+    publicFunctions.users.del       = function(usernaem){
 
     };
-    publicFunctions.incr    = function(key){
+    publicFunctions.users.delById   = function(id){
 
     };
-    publicFunctions.incrBy  = function(key,value){
+
+    publicFunctions.contests        = Object();
+    publicFunctions.contests.new    = function(){
+
+    };
+    publicFunctions.contests.set    = function(key,value){
+
+    };
+    publicFunctions.contests.get    = function(name){
+
+    };
+    publicFunctions.contests.getById= function(id){
+
+    };
+    publicFunctions.contests.list   = function(){
+
+    };
+    publicFunctions.contests.del    = function(name){
+
+    };
+    publicFunctions.contests.delById= function(id){
+
+    };
+
+    publicFunctions.questions       = Object();
+    publicFunctions.questions.new   = function(){
+
+    };
+    publicFunctions.questions.set   = function(key,value){
+
+    };
+    publicFunctions.questions.get   = function(id){
+
+    };
+    publicFunctions.questions.del   = function(id){
+
+    };
+
+    publicFunctions.log             = Object();
+    publicFunctions.log.new         = function(){
+
+    };
+    publicFunctions.log.set         = function(key,vlaue){
+
+    };
+    publicFunctions.log.get         = function(id){
+
+    };
+    publicFunctions.log.del         = function(id){
+
+    };
+    publicFunctions.log.list        = function(){
+
+    };
+
+    publicFunctions.message         = Object();
+    publicFunctions.message.new     = function(){
+
+    };
+    publicFunctions.message.set     = function(key,value){
+
+    };
+    publicFunctions.message.get     = function(id){
+
+    };
+    publicFunctions.message.del     = function(id){
+
+    };
+    publicFunctions.message.list    = function(){
+
+    };
+
+    publicFunctions.langs           = Object();
+    publicFunctions.langs.set       = function(key,value){
+
+    };
+    publicFunctions.langs.get       = function(key) {
+
+    };
+    publicFunctions.langs.del       = function(key){
 
     };
     return publicFunctions;
