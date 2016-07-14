014954b273ab60c7ef17d5fb808e56cd62ef39bc
Qti3e <Alirezaghadimy@gmail.com>
2016-07-14 20:41:53 +0430

    cd command (change directory)

---
+++ server/lib/sysadmin/commands/cd.php
@@ -0,0 +1,66 @@
+<?php
+/*****************************************************************************
+ *         In the name of God the Most Beneficent the Most Merciful          *
+ *___________________________________________________________________________*
+ *   This program is free software: you can redistribute it and/or modify    *
+ *   it under the terms of the GNU General Public License as published by    *
+ *   the Free Software Foundation, either version 3 of the License, or       *
+ *   (at your option) any later version.                                     *
+ *___________________________________________________________________________*
+ *   This program is distributed in the hope that it will be useful,         *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of          *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           *
+ *   GNU General Public License for more details.                            *
+ *___________________________________________________________________________*
+ *   You should have received a copy of the GNU General Public License       *
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.   *
+ *___________________________________________________________________________*
+ *                             Created by  Qti3e                             *
+ *        <http://Qti3e.Github.io>    LO-VE    <Qti3eQti3e@Gmail.com>        *
+ *****************************************************************************/
+namespace lib\sysadmin\commands;
+
+use lib\helper\validation;
+use lib\network\Socket;
+use lib\network\WebSocketUser;
+
+/**
+ * Class cd
+ * @package lib\sysadmin\commands
+ */
+class cd{
+	/**
+	 * @var bool
+	 */
+	public static $needLogin    = true;
+
+	/**
+	 * @param $param
+	 * @param WebSocketUser $user
+	 *
+	 * @return string
+	 */
+	public static function process($param,$user){
+		$dir = isset($user->information['dir']) ? $user->information['dir'] : './';
+		if(isset($param[0])){
+			if(substr($param[0],-1) !== '/'){
+				$param[0]   = $param[0].'/';
+			}
+			if(substr($param[0],0,1) == '/'){
+				$param[0]   = substr($param[0],1);
+			}
+			if(!validation::validate_dir($param[0])){
+				return 'Please enter a valid directory name.';
+			}
+			if(!file_exists($dir.$param[0])){
+				return 'Directory does not exist!';
+			}
+			if(filetype($dir.$param[0]) != 'dir'){
+				return 'Your entered place is not a directory!';
+			}
+			$dir    .=  $param[0];
+		}
+		$user->information['dir']   = $dir;
+		return $dir;
+	}
+}
